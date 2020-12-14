12001731062c8fda0102cfcd9737bfa4ffab2ab6
SomannaAA12 <67817939+SomannaAA12@users.noreply.github.com>
2020-12-14 11:42:36 +0530

    Switch case
    Project for My Captain Java course.

---
+++ Task3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args){
+    int choice;
+    System.out.println("Select your choice: 1. Hi\t2. Hey\t3. Hello\t");
+    Scanner s = new Scanner(System.in);
+    choice = s.nextInt();
+    switch(choice)
+    {
+    case 1 : System.out.println("You said Hi.");
+           break;
+    case 2 : System.out.println("You said Hey.");
+           break;
+    case 3 : System.out.println("You said Hello.");
+          break;
+    default : System.out.println("Invalid Choice.");
+    }
+    }
+    
+   
+}
